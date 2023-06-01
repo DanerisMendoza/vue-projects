fa178edf61aa2f690a87371306bae0bb97f3fee4
Daneris <danerismendoza096@gmail.com>
2023-06-01 22:02:34 +0800

    v-for, v-if

--- vueIntro/app.js
+++ vueIntro/app.js
@@ -10,13 +10,29 @@ const app = Vue.createApp({
         return {
             title: 'The litle Giant',
             author: 'Bulfo Macaren',
-            age: 25
+            age: 25,
+            condition: true,
+            books: [
+                {title: 'Unconditional love', author: 'kim soo', isFave: true},
+                {title: 'love the way you are', author: 'pawarit', isFave: false},
+                {title: 'life is hard', author: 'vincent lancough', isFave: true},
+            ]
         }
     },
+    
     methods: {
         resetTitle(){
             // alert('hey');
             this.title = 'The litle Giant';
+        },
+        changeTitleThroughParam(title){
+            this.title = title;
+        },
+    },
+    
+    computed: {
+        filteredBooks(){
+            return this.books.filter((book) => book.isFave)
         }
     }
 });
